7a9057c518e9175480f642493b6f7b451757b906
Kindeme <fkounoudji@gmail.com>
2020-10-30 20:31:38 -0500

    using jsx and props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,40 +3,69 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
-// stateless functional component
 // always return JSX
 
-// nested Components, react tools
+//setup vars
+const firstBook = {
+	img:
+		"https://images-na.ssl-images-amazon.com/images/I/71tPmybVKxL._AC_UL200_SR200,200_.jpg",
+	title: "Fortune and Glory",
+	author: "Janet Evanovich",
+};
+
+const secondBook = {
+	img:
+		"https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+	title: "A Promised Land",
+	author: "Barack Obama",
+};
+
+// stateless functional component
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
+			<Book
+				img={firstBook.img}
+				title={firstBook.title}
+				author={firstBook.author}
+			/>
+			<Book
+				img={secondBook.img}
+				title={secondBook.title}
+				author={secondBook.author}
+			/>
 		</section>
 	);
 }
-const Book = () => {
+
+// Normal app writing
+const Book = (props) => {
 	return (
-		<article className='book'>
-			<Image></Image>
-			<Title />
-			<Author />
+		<article className="book">
+			<img src={props.img} alt="" />
+			<h1>{props.title} </h1>
+			<h4>{props.author}</h4>
 		</article>
 	);
 };
-const Image = () => (
-	<img
-		src="https://images-na.ssl-images-amazon.com/images/I/71tPmybVKxL._AC_UL200_SR200,200_.jpg"
-		alt=""
-	/>
-);
+// nested Components, react tools
+// const Book = () => {
+// 	return (
+// 		<article className='book'>
+// 			<Image></Image>
+// 			<Title />
+// 			<Author />
+// 		</article>
+// 	);
+// };
+// const Image = () => (
+// 	<img
+// 		src="https://images-na.ssl-images-amazon.com/images/I/71tPmybVKxL._AC_UL200_SR200,200_.jpg"
+// 		alt=""
+// 	/>
+// );
 //  JSX
-const Title = () => <h1>Fortune and Glory: A Novel </h1>;
-const Author = () => <h4 syle={{color:'#617d98', fontSize: '0.75rem', marginTop: '0.75rem'}}>Janet Evanovich </h4>;
+// const Title = () => <h1>Fortune and Glory: A Novel </h1>;
+// const Author = () => <h4 style={{color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Janet Evanovich </h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
