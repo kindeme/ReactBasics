80900b5583a8282a2b4dbd168ef026ccfa863d26
Kindeme <fkounoudji@gmail.com>
2020-10-30 23:48:24 -0500

    List Key prop and spread operator

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,45 +6,51 @@ import "./index.css";
 // always return JSX
 
 //setup vars
-const firstBook = {
-	img:
-		"https://images-na.ssl-images-amazon.com/images/I/71tPmybVKxL._AC_UL200_SR200,200_.jpg",
-	title: "Fortune and Glory",
-	author: "Janet Evanovich",
-};
+const books = [
+	{
+		id: 1,
+		img:
+			"https://images-na.ssl-images-amazon.com/images/I/71tPmybVKxL._AC_UL200_SR200,200_.jpg",
+		title: "Fortune and Glory",
+		author: "Janet Evanovich",
+	},
 
-const secondBook = {
-	img:
-		"https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
-	title: "A Promised Land",
-	author: "Barack Obama",
-};
+	{
+		id: 2,
+		img:
+			"https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+		title: "A Promised Land",
+		author: "Barack Obama",
+	},
+	{
+		id: 3,
+		img:
+			"https://images-na.ssl-images-amazon.com/images/I/A1i8NcG05pL._AC_UL200_SR200,200_.jpg",
+		title: "A Time for Mercy (Jake Brigance)",
+		author: "John Grisham",
+	},
+];
 
 // stateless functional component
+
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
-			/>
-			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author}
-			/>
+			{books.map((book,index) => {
+				return <Book key={book.id} {...book}></Book>;
+			})}
 		</section>
 	);
 }
 
 // Normal app writing
 const Book = (props) => {
+	const { img, title, author } = props;
 	return (
 		<article className="book">
-			<img src={props.img} alt="" />
-			<h1>{props.title} </h1>
-			<h4>{props.author}</h4>
+			<img src={img} alt="" />
+			<h1>{title} </h1>
+			<h4>{author}</h4>
 		</article>
 	);
 };
